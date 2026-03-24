edbcc0262e0339de254654a12471de02348475f8
1811ts-boop <1811ts@gmail.com>
2026-03-24 20:21:19 +0900

    Update V7_DailyRun.py

--- V7_DailyRun.py
+++ V7_DailyRun.py
@@ -270,7 +270,7 @@ def transform_for_inference_v7(df_raw, df_tide):
                 raw_pc = row.get(f"Boat{b}_Past_{i}_Course")
 
                 pst, prk, pc = safe_float(raw_pst), safe_float(raw_prk), safe_float(raw_pc)
-                w = max(0.2, 1.6 - (i * 0.1))
+                w = max(0.2, (15 - i) * 0.1)
                 
                 mm_s1 += get_rank_point_s1(raw_prk) * w
                 if prk > 0: mm_s2 += get_rank_point_s2(raw_prk) * w
